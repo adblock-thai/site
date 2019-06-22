fc36d95eb038bcd11de32025d581c3d8b09811a8
pakonda <pakondaman@gmail.com>
2019-06-22 08:29:35 +0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Adblock Thai Site
+[![Build Status](https://travis-ci.org/adblock-thai/site.svg?branch=master)](https://travis-ci.org/adblock-thai/site)
--- config/theme.js
+++ config/theme.js
@@ -3,7 +3,7 @@ import { lighten } from "polished";
 const colors = {
   primary: "#d02e77", // Color for buttons or links
   primaryLight: lighten(0.05, "#d02e77"),
-  bg: "#F6EFEF", // Background color
+  bg: "#f6efef", // Background color
   grey: {
     dark: "rgba(0, 0, 0, 0.9)",
     default: "rgba(0, 0, 0, 0.7)",
