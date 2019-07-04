d3d5f637e343ca1385d143b1d0845c55d727b6ee
pakonda <pakondaman@gmail.com>
2019-07-04 09:00:22 +0700

    disable plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,20 +57,20 @@ module.exports = {
     "gatsby-plugin-catch-links",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-lodash",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: config.siteTitleAlt,
-        short_name: config.siteTitleManifest,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: "standalone",
-        icon: config.favicon
-      }
-    },
-    "gatsby-plugin-offline",
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     name: config.siteTitleAlt,
+    //     short_name: config.siteTitleManifest,
+    //     description: config.siteDescription,
+    //     start_url: config.pathPrefix,
+    //     background_color: config.backgroundColor,
+    //     theme_color: config.themeColor,
+    //     display: "standalone",
+    //     icon: config.favicon
+    //   }
+    // },
+    // "gatsby-plugin-offline",
     "gatsby-plugin-netlify"
   ]
 };
