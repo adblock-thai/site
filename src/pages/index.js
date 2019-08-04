e2291a21d48174b708fab6c6ad2cefb4ab43e79d
pakonda <pakondaman@gmail.com>
2019-08-04 10:40:57 +0700

    fix descendant

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,8 @@ const Hero = styled.div`
     padding: 2rem 1rem 4rem 1rem;
   }
 
-  p {
+  p,
+  ul {
     font-size: 1.68rem;
     margin-top: -1rem;
     @media (max-width: ${props => props.theme.breakpoints.tablet}) {
@@ -58,79 +59,79 @@ const IndexPage = ({
         </p>
         <hr />
         <br />
+
+        <div className="is-size-4">
+          <p>
+            ติดตั้งส่วนเสริม{" "}
+            <span className="is-dark is-top is-large tooltip is-multiline">
+              <a
+                href="https://github.com/gorhill/uBlock/#installation"
+                target="_blank"
+              >
+                uBlock Origin
+              </a>
+            </span>{" "}
+            หรือ{" "}
+            <span className="is-dark is-top is-large tooltip is-multiline">
+              <a
+                href="https://github.com/NanoAdblocker/NanoCore#install-links"
+                target="_blank"
+              >
+                Nano Adblocker
+              </a>
+            </span>
+          </p>
+          <ul>
+            <li>
+              <a
+                href="/วิธีติดตั้งโปรแกรมบล็อกโฆษณาสำหรับ-chrome"
+                className="button is-success is-rounded"
+              >
+                วิธีติดตั้งสำหรับ Chrome
+              </a>
+            </li>
+            <li>
+              <a
+                href="/วิธีติดตั้งโปรแกรมบล็อกโฆษณาสำหรับ-firefox-ใช้ใน-android-ได้"
+                className="button is-success is-rounded"
+              >
+                วิธีติดตั้งสำหรับ Firefox
+              </a>
+            </li>
+          </ul>
+          <p>
+            ถ้ามี Adblock ติดตั้งอยู่แล้ว{" "}
+            <a href="abp:subscribe?location=https://adblock-thai.github.io/thai-ads-filter/subscription.txt&title=Thai Adblock List">
+              กดที่นี่
+            </a>{" "}
+            เพื่อเพิ่มรายการไปยังส่วนเสริม
+            <br />
+            หรือเพิ่ม{" "}
+            <a href="https://adblock-thai.github.io/thai-ads-filter/subscription.txt">
+              URL
+            </a>{" "}
+            ด้วยตัวเอง
+          </p>
+        </div>
+
         <p>
-          <div className="is-size-4">
-            <p>
-              ติดตั้งส่วนเสริม{" "}
-              <span className="is-dark is-top is-large tooltip is-multiline">
-                <a
-                  href="https://github.com/gorhill/uBlock/#installation"
-                  target="_blank"
-                >
-                  uBlock Origin
-                </a>
-              </span>{" "}
-              หรือ{" "}
-              <span className="is-dark is-top is-large tooltip is-multiline">
-                <a
-                  href="https://github.com/NanoAdblocker/NanoCore#install-links"
-                  target="_blank"
-                >
-                  Nano Adblocker
-                </a>
-              </span>
-            </p>
-            <p>
-              <ul>
-                <li>
-                  <a
-                    href="/วิธีติดตั้งโปรแกรมบล็อกโฆษณาสำหรับ-chrome"
-                    className="button is-success is-rounded"
-                  >
-                    วิธีติดตั้งสำหรับ Chrome
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/วิธีติดตั้งโปรแกรมบล็อกโฆษณาสำหรับ-firefox-ใช้ใน-android-ได้"
-                    className="button is-success is-rounded"
-                  >
-                    วิธีติดตั้งสำหรับ Firefox
-                  </a>
-                </li>
-              </ul>
-            </p>
-            <p>
-              ถ้ามี Adblock ติดตั้งอยู่แล้ว{" "}
-              <a href="abp:subscribe?location=https://adblock-thai.github.io/thai-ads-filter/subscription.txt&title=Thai Adblock List">
-                กดที่นี่
-              </a>{" "}
-              เพื่อเพิ่มรายการไปยังส่วนเสริม
-              <br />
-              หรือเพิ่ม{" "}
-              <a href="https://adblock-thai.github.io/thai-ads-filter/subscription.txt">
-                URL
-              </a>{" "}
-              ด้วยตัวเอง
-            </p>
-          </div>
+          <Link to="/report">
+            พบโฆษณาในเว็บ?{"  "}
+            <Button small>
+              <svg
+                width="1792"
+                height="1792"
+                viewBox="0 0 1792 1792"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
+              </svg>
+              แจ้งที่นี่
+            </Button>
+          </Link>
         </p>
         <hr />
         <br />
-        <Link to="/report">
-          พบโฆษณาในเว็บ?{"  "}
-          <Button small>
-            <svg
-              width="1792"
-              height="1792"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-            </svg>
-            แจ้งที่นี่
-          </Button>
-        </Link>
       </Hero>
       <Content>
         <SectionTitle>Latest stories</SectionTitle>
