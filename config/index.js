b8d1b6e58ff5f1722edf3e07f050abefd98c2fce
pakonda <pakondaman@gmail.com>
2019-08-04 23:02:57 +0700

    correct word

--- config/index.js
+++ config/index.js
@@ -2,15 +2,15 @@ module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: "Adblock Thai", // Navigation and Site Title
-  siteTitleAlt: "Adblock Thai บล็อคโฆษณา ลบโฆษณา", // Alternative Site title for SEO
+  siteTitleAlt: "Adblock Thai บล็อกโฆษณา ลบโฆษณา", // Alternative Site title for SEO
   siteTitleManifest: "Adblock Thai",
   siteUrl: "https://adblock-thai.github.io", // Domain of your site. No trailing slash!
   siteLanguage: "th", // Language Tag on <html> element
   siteHeadline: "รายการตัวกรองโฆษณาสำหรับประเทศไทย", // Headline for schema.org JSONLD
   siteBanner: "/social/banner.jpg", // Your image for og:image tag. You can find it in the /static folder
   favicon: "src/favicon.png", // Your image for favicons. You can find it in the /src folder
-  siteDescription: "บล็อคโฆษณาและการติดตามต่างๆในเว็บของประเทศไทย", // Your site description
-  siteKeyword: "ลบโฆษณา,บล็อคโฆษณา,adblock,adblock thai,block ads", // Your site keyword
+  siteDescription: "บล็อกโฆษณาและการติดตามต่างๆในเว็บของประเทศไทย", // Your site description
+  siteKeyword: "ลบโฆษณา,บล็อกโฆษณา,adblock,adblock thai,block ads", // Your site keyword
   author: "adblock-thai", // Author for schemaORGJSONLD
   siteLogo: "/social/logo.png", // Image for schemaORGJSONLD
 
